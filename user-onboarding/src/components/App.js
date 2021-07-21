5021ecf598904af1733b0745e9cc5bf5762f2cc7
Andrewschaer <andrewschaer@github.com>
2021-07-21 18:58:52 -0400

    Updates made to App.js: Initial States Added, State Declarations Made, Helpfer Functions Declared, Event Handlers Included, Side Effects Included, Props included for Form Component

--- user-onboarding/src/components/App.js
+++ user-onboarding/src/components/App.js
@@ -1,43 +1,111 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Form from './Form.js'
 import schema from '../validation/formSchema.js'
-import * as yup from 'yup'
+import { reach } from 'yup'
 import '../index.css';
 
 // Initial States
-// - [ ] Name
-// - [ ] Email
-// - [ ] Password
-// - [ ] Terms of Service (checkbox)
-// - [ ] A Submit button to send our form data to the server.
-const initialFormValues = {
 
+const initialFormValues = {
+  // Text Inputs
+  name: '',
+  username: '',
+  email: '',
+  // Password Inputs
+  password: '',
+  // Dropdown Inputs
+  userRole: '',
+  // Date Inputs
+  birthday: '',
+  // File Inputs
+  profilePic: '',
+  // Checkbox Inputs
+  tos: '',
 }
+
+const initialFormErrors = {
+  name: '',
+  username: '',
+  email: '',
+  password: '',
+  userRole: '',
+  birthday: '',
+  tos: '',
+}
+
+const initialUsers = []
+const initialDisabled = true
 
 function App() {
   // State Declarations
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
 
   // Helper Functions
 // - [ ] Craft a `POST` request using `axios` that sends your form data to the following endpoint: _`https://reqres.in/api/users`_
 // - [ ] Verify using a `console.log()` that you are receiving a successful response back
+  const postNewUser = newUser => {
+    axios.post('https://reqres.in/api/users', newUser)
+      .then (res => {
+        setUsers([res.data, ...users])
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      })
+  }
+
+  const validate = (name, value) => {
+    reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  }
+
   // Event Handlers
+
+  const inputChange = (name, value) => {
+    validate(name, value)
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const formSubmit = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      userRole: formValues.userRole.trim(),
+      birthday: formValues.birthday.trim(),
+      tos: formValues.tos.trim(),
+      profilePic: formValues.profilePic.trim(),
+    }
+    postNewUser(newUser)
+  }
 
   // Side Effects
 
-
+  useEffect(() => {
+    schema.isValid(formValues).then(valid => setDisabled(!valid))
+  }, [formValues])
 
   return (
     <div className="App">
       <header><h1>User-Onboarding Form</h1></header>
       <Form 
-        values={''}
-        change={''}
-        submit={''}
-        disabled={''}
-        errors={''}
-
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
       />
       <pre>
         {JSON.stringify(users)}
